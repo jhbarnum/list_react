0f8baaa36f812c42d862e899289972568a5ea464
Jared <jhbarnum@gmail.com>
2018-12-31 06:52:27 -0500

    output

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import './App.css';
 
 class App extends Component {
   
+  state = {
+    
+    letterCount: 
+      { count: 0 }
+    
+  }
+
   inputCountHandler = (event, inputCount) => {
-
     let wordArray = event.target.value;
-
-  console.log('counting ' + wordArray.length + ' ' + wordArray)
-}
+    console.log('counting ' + wordArray.length + ' ' + wordArray)
+  }
 
   render() {
-let count = (
-  <p>Hello</p>
-)
+    let displayCount = (
+    <p>{this.letterCount} K</p>
+    )
 
-    return (
-      <div className="App">
-        <input
-        onChange={this.inputCountHandler}></input>
+  return (
+    <div className="App">
+      <input
+      onChange={this.inputCountHandler}></input>
 
-        <div>{count}</div>
-      </div>
-    );
+      <div>{displayCount}</div>
+    </div>
+  );
   }
 }
 
